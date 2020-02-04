098f5f6f9cf14fd1bf8a970d5789c85721c07c92
snpr <vaugk13@gmail.com>
2020-02-04 16:36:30 +0500

    Create Movie enum with movie description; Pass props to App component

--- src/index.js
+++ src/index.js
@@ -2,4 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/app/app.jsx";
 
-ReactDOM.render(<App />, document.querySelector(`#root`));
+const Movie = {
+  NAME: `The Grand Budapest Hotel`,
+  GENRE: `Drama`,
+  RELEASE_YEAR: 2014,
+  POSTER_URL: `img/the-grand-budapest-hotel-poster.jpg`
+};
+
+ReactDOM.render(
+    <App
+      name={Movie.NAME}
+      genre={Movie.GENRE}
+      releaseYear={Movie.RELEASE_YEAR}
+      posterUrl={Movie.POSTER_URL}
+    />,
+    document.querySelector(`#root`)
+);
