864cb056c612159c854444310093ca3ed6fb6c98
snpr <vaugk13@gmail.com>
2020-02-11 14:27:29 +0500

    Update existing tests

--- src/components/main/main.e2e.test.js
+++ src/components/main/main.e2e.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Enzyme, {shallow} from "enzyme";
+import Enzyme, {mount} from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 import Main from "./main.jsx";
 
@@ -13,21 +13,38 @@ const Movie = {
   RELEASE_YEAR: 1960
 };
 
-const MOVIES = [
-  `Schindler's List`,
-  `Forrest Gump`,
-  `The Lord of the Rings: The Return of the King`
+const films = [
+  {
+    name: `Star Wars: A New Hope`,
+    posterUrl: `https://www.gstatic.com/tv/thumb/v22vodart/4407/p4407_v_v8_be.jpg/images?q=tbn:ANd9GcSkmMH-bEDUS2TmK8amBqgIMgrfzN1_mImChPuMrunA1XjNTSKm`
+  },
+  {
+    name: `2001: A Space Odyssey`,
+    posterUrl: `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQdmmLZ7lXsw1WRy7c5qN3mka2e9ANSpHIG2INi53P6OVS8KyJo/images?q=tbn:ANd9GcRz_2nKTNlxhVtzbh29kgL3m2ebLv3TlYyzrbyqBtEUxt6mBuZ-`
+  },
+  {
+    name: `Goodfellas`,
+    posterUrl: `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkuxYKBhyPQq4e_cbYRDfZRjWkUx2GIKlUpUkHiuVeLg2GhN0D`
+  },
+  {
+    name: `Taxi Driver`,
+    posterUrl: `https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQ4QYpWCQ5pareSyOO3YNI3YsOmAeF4qcRDwHCDwf3-a3F0Y7N6/images?q=tbn:ANd9GcTLgFST-pqPgU9CcWUDE4xqqgSxB1n7dulszo08FjRyXyCxV8TH`
+  },
+  {
+    name: `Casablanca`,
+    posterUrl: `https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRlIHtgWeQQsiZaY5iw_A5FTDeKr7bNFWo1W_zObm0h_ti2Yk0V`
+  }
 ];
 
 it(`Should welcome button be pressed`, () => {
   const movieTitleClickHandler = jest.fn();
 
-  const main = shallow(
+  const main = mount(
       <Main
         name={Movie.NAME}
         genre={Movie.GENRE}
         releaseYear={Movie.RELEASE_YEAR}
-        movies={MOVIES}
+        movies={films}
         onMovieTitleClick={movieTitleClickHandler}
       />
   );
--- src/components/main/main.test.js
+++ src/components/main/main.test.js
@@ -8,7 +8,28 @@ const Movie = {
   RELEASE_YEAR: 1972
 };
 
-const MOVIES = [`The Dark Knight`, `The Shawshank Redemption`, `Pulp Fiction`];
+const films = [
+  {
+    name: `Star Wars: A New Hope`,
+    posterUrl: `https://www.gstatic.com/tv/thumb/v22vodart/4407/p4407_v_v8_be.jpg/images?q=tbn:ANd9GcSkmMH-bEDUS2TmK8amBqgIMgrfzN1_mImChPuMrunA1XjNTSKm`
+  },
+  {
+    name: `2001: A Space Odyssey`,
+    posterUrl: `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQdmmLZ7lXsw1WRy7c5qN3mka2e9ANSpHIG2INi53P6OVS8KyJo/images?q=tbn:ANd9GcRz_2nKTNlxhVtzbh29kgL3m2ebLv3TlYyzrbyqBtEUxt6mBuZ-`
+  },
+  {
+    name: `Goodfellas`,
+    posterUrl: `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkuxYKBhyPQq4e_cbYRDfZRjWkUx2GIKlUpUkHiuVeLg2GhN0D`
+  },
+  {
+    name: `Taxi Driver`,
+    posterUrl: `https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQ4QYpWCQ5pareSyOO3YNI3YsOmAeF4qcRDwHCDwf3-a3F0Y7N6/images?q=tbn:ANd9GcTLgFST-pqPgU9CcWUDE4xqqgSxB1n7dulszo08FjRyXyCxV8TH`
+  },
+  {
+    name: `Casablanca`,
+    posterUrl: `https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRlIHtgWeQQsiZaY5iw_A5FTDeKr7bNFWo1W_zObm0h_ti2Yk0V`
+  }
+];
 
 it(`Should render App component`, () => {
   const tree = renderer
@@ -17,7 +38,7 @@ it(`Should render App component`, () => {
           name={Movie.NAME}
           genre={Movie.GENRE}
           releaseYear={Movie.RELEASE_YEAR}
-          movies={MOVIES}
+          movies={films}
           onMovieTitleClick={() => {}}
         />
     )
