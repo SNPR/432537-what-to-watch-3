51e891cfbb7c928a6660f7dc757326467b465ebb
snpr <vaugk13@gmail.com>
2020-02-11 14:04:16 +0500

    Pass films array to App component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/app/app.jsx";
+import films from "./mocks/films";
 
 const Movie = {
   NAME: `The Grand Budapest Hotel`,
@@ -8,14 +9,12 @@ const Movie = {
   RELEASE_YEAR: 2014
 };
 
-const MOVIES = [`The Shawshank Redemption`, `Pulp Fiction`, `Star Wars`];
-
 ReactDOM.render(
     <App
       name={Movie.NAME}
       genre={Movie.GENRE}
       releaseYear={Movie.RELEASE_YEAR}
-      movies={MOVIES}
+      movies={films}
     />,
     document.querySelector(`#root`)
 );
