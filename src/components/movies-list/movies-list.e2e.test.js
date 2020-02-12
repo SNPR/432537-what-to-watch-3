2e7d888e4fb28fa660e80d35547330555f0bb6ad
snpr <vaugk13@gmail.com>
2020-02-12 17:19:25 +0500

    Add e2e-test for MoviesList component

---
+++ src/components/movies-list/movies-list.e2e.test.js
@@ -0,0 +1,119 @@
+import React from "react";
+import Enzyme, {mount} from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import MoviesList from "./movies-list.jsx";
+
+Enzyme.configure({
+  adapter: new Adapter()
+});
+
+const films = [
+  {
+    name: `Movie name`,
+    posterUrl: `https://poster-url.com`,
+    bigPosterUrl: `https://image-url.com/1.jpg`,
+    director: `Director Name`,
+    starring: [`Actor 1`, `Actor 2`, `Actor 3`],
+    runTime: `1h 00m`,
+    genre: `Movie Genre`,
+    releaseYear: 2000,
+    rating: 8.9,
+    votes: 4235,
+    description: `Movie description`
+  },
+  {
+    name: `Movie name`,
+    posterUrl: `https://poster-url.com`,
+    bigPosterUrl: `https://image-url.com/1.jpg`,
+    director: `Director Name`,
+    starring: [`Actor 1`, `Actor 2`, `Actor 3`],
+    runTime: `1h 00m`,
+    genre: `Movie Genre`,
+    releaseYear: 2000,
+    rating: 8.9,
+    votes: 4235,
+    description: `Movie description`
+  },
+  {
+    name: `Movie name`,
+    posterUrl: `https://poster-url.com`,
+    bigPosterUrl: `https://image-url.com/1.jpg`,
+    director: `Director Name`,
+    starring: [`Actor 1`, `Actor 2`, `Actor 3`],
+    runTime: `1h 00m`,
+    genre: `Movie Genre`,
+    releaseYear: 2000,
+    rating: 8.9,
+    votes: 4235,
+    description: `Movie description`
+  },
+  {
+    name: `Movie name`,
+    posterUrl: `https://poster-url.com`,
+    bigPosterUrl: `https://image-url.com/1.jpg`,
+    director: `Director Name`,
+    starring: [`Actor 1`, `Actor 2`, `Actor 3`],
+    runTime: `1h 00m`,
+    genre: `Movie Genre`,
+    releaseYear: 2000,
+    rating: 8.9,
+    votes: 4235,
+    description: `Movie description`
+  },
+  {
+    name: `Movie name`,
+    posterUrl: `https://poster-url.com`,
+    bigPosterUrl: `https://image-url.com/1.jpg`,
+    director: `Director Name`,
+    starring: [`Actor 1`, `Actor 2`, `Actor 3`],
+    runTime: `1h 00m`,
+    genre: `Movie Genre`,
+    releaseYear: 2000,
+    rating: 8.9,
+    votes: 4235,
+    description: `Movie description`
+  },
+  {
+    name: `Movie name`,
+    posterUrl: `https://poster-url.com`,
+    bigPosterUrl: `https://image-url.com/1.jpg`,
+    director: `Director Name`,
+    starring: [`Actor 1`, `Actor 2`, `Actor 3`],
+    runTime: `1h 00m`,
+    genre: `Movie Genre`,
+    releaseYear: 2000,
+    rating: 8.9,
+    votes: 4235,
+    description: `Movie description`
+  },
+  {
+    name: `Movie name`,
+    posterUrl: `https://poster-url.com`,
+    bigPosterUrl: `https://image-url.com/1.jpg`,
+    director: `Director Name`,
+    starring: [`Actor 1`, `Actor 2`, `Actor 3`],
+    runTime: `1h 00m`,
+    genre: `Movie Genre`,
+    releaseYear: 2000,
+    rating: 8.9,
+    votes: 4235,
+    description: `Movie description`
+  }
+];
+
+it(`Should pass data to handler on click`, () => {
+  const movieCardClickHandler = jest.fn();
+
+  const moviesList = mount(
+      <MoviesList movies={films} onMovieCardClick={movieCardClickHandler} />
+  );
+
+  const movieCard = moviesList
+    .find(`article.small-movie-card.catalog__movies-card`)
+    .first();
+
+  movieCard.simulate(`click`);
+
+  expect(movieCardClickHandler.mock.calls.length).toBe(1);
+  expect(movieCardClickHandler.mock.calls[0][0]).toBe(0);
+});
