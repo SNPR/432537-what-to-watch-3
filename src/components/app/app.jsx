12725f3bbf1090320def532f8e6b15e9b6ed376d
snpr <vaugk13@gmail.com>
2020-02-12 16:28:48 +0500

    Add _renderApp method; Render movie details on movie card click

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,6 +1,6 @@
-import React, { PureComponent } from "react";
+import React, {PureComponent} from "react";
 import PropTypes from "prop-types";
-import { Switch, Route, BrowserRouter } from "react-router-dom";
+import {Switch, Route, BrowserRouter} from "react-router-dom";
 import Main from "../main/main.jsx";
 import MovieDetails from "../movie-details/movie-details.jsx";
 
@@ -8,27 +8,46 @@ class App extends PureComponent {
   constructor(props) {
     super(props);
 
+    this.state = {
+      selectedMovieId: null
+    };
     this.movieCardClickHandler = this.movieCardClickHandler.bind(this);
   }
 
-  movieCardClickHandler() {}
+  movieCardClickHandler(selectedMovieId) {
+    this.setState({selectedMovieId});
+  }
+
+  _renderApp() {
+    const {genre, releaseYear, movies} = this.props;
+    const {selectedMovieId} = this.state;
+
+    if (selectedMovieId) {
+      return <MovieDetails movie={this.props.movies[selectedMovieId]} />;
+    }
+
+    return (
+      <Main
+        name={name}
+        genre={genre}
+        releaseYear={releaseYear}
+        movies={movies}
+        onMovieCardClick={this.movieCardClickHandler}
+      />
+    );
+  }
 
   render() {
-    const { name, genre, releaseYear, movies } = this.props;
     return (
       <BrowserRouter>
         <Switch>
           <Route exact path="/">
-            <Main
-              name={name}
-              genre={genre}
-              releaseYear={releaseYear}
-              movies={movies}
-              onMovieCardClick={this.movieCardClickHandler}
-            />
+            {this._renderApp()}
           </Route>
           <Route exact path="/dev-movie-details">
-            <MovieDetails movie={movies[1]} />
+            <MovieDetails
+              movie={this.props.movies[this.state.selectedMovieId || 0]}
+            />
           </Route>
         </Switch>
       </BrowserRouter>
@@ -41,19 +60,19 @@ App.propTypes = {
   genre: PropTypes.string.isRequired,
   releaseYear: PropTypes.number.isRequired,
   movies: PropTypes.arrayOf(
-    PropTypes.shape({
-      name: PropTypes.string.isRequired,
-      posterUrl: PropTypes.string.isRequired,
-      bigPosterUrl: PropTypes.string.isRequired,
-      director: PropTypes.string.isRequired,
-      starring: PropTypes.arrayOf(PropTypes.string).isRequired,
-      runTime: PropTypes.string.isRequired,
-      genre: PropTypes.string.isRequired,
-      releaseYear: PropTypes.number.isRequired,
-      rating: PropTypes.number.isRequired,
-      votes: PropTypes.number.isRequired,
-      description: PropTypes.string.isRequired
-    }).isRequired
+      PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        posterUrl: PropTypes.string.isRequired,
+        bigPosterUrl: PropTypes.string.isRequired,
+        director: PropTypes.string.isRequired,
+        starring: PropTypes.arrayOf(PropTypes.string).isRequired,
+        runTime: PropTypes.string.isRequired,
+        genre: PropTypes.string.isRequired,
+        releaseYear: PropTypes.number.isRequired,
+        rating: PropTypes.number.isRequired,
+        votes: PropTypes.number.isRequired,
+        description: PropTypes.string.isRequired
+      }).isRequired
   ).isRequired
 };
 
