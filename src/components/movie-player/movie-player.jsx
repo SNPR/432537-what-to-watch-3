4971c03ee206fc695f8c0e340512e057ff1d83e7
snpr <vaugk13@gmail.com>
2020-02-18 15:06:17 +0500

    Create MoviePlayer component

---
+++ src/components/movie-player/movie-player.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const MoviePlayer = ({movie, muted}) => {
+  return (
+    <video muted={muted} controls poster={movie.posterUrl} width="280" autoPlay>
+      <source src={movie.trailerUrl} />
+    </video>
+  );
+};
+
+MoviePlayer.propTypes = {
+  movie: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    posterUrl: PropTypes.string.isRequired,
+    bigPosterUrl: PropTypes.string.isRequired,
+    trailerUrl: PropTypes.string.isRequired,
+    director: PropTypes.string.isRequired,
+    starring: PropTypes.arrayOf(PropTypes.string).isRequired,
+    runTime: PropTypes.string.isRequired,
+    genre: PropTypes.string.isRequired,
+    releaseYear: PropTypes.number.isRequired,
+    rating: PropTypes.number.isRequired,
+    votes: PropTypes.number.isRequired,
+    description: PropTypes.string.isRequired
+  }).isRequired,
+  muted: PropTypes.bool.isRequired
+};
+
+export default MoviePlayer;
