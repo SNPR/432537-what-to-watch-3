8cf17b79c75433e8f2b94fb664f3cbb0ddf2390b
snpr <vaugk13@gmail.com>
2020-02-19 23:16:25 +0500

    Add propTypes for SimiLarMovies component

--- src/components/similar-movies/similar-movies.jsx
+++ src/components/similar-movies/similar-movies.jsx
@@ -1,20 +1,68 @@
 import React from "react";
 import MoviesList from "../movies-list/movies-list.jsx";
+import PropTypes from "prop-types";
 
 const getSimilarMovies = (movies, movie) => {
   return movies.filter(
-    similarMovie =>
-      similarMovie.genre === movie.genre && similarMovie.name !== movie.name
+      (similarMovie) =>
+        similarMovie.genre === movie.genre && similarMovie.name !== movie.name
   );
 };
 
-const SimilarMovies = ({ movies, movie }) => {
+const SimilarMovies = ({movies, movie}) => {
   return (
     <MoviesList
       movies={getSimilarMovies(movies, movie)}
       onMovieCardClick={() => {}}
     />
   );
+};
+
+SimilarMovies.propTypes = {
+  movies: PropTypes.arrayOf(
+      PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        posterUrl: PropTypes.string.isRequired,
+        bigPosterUrl: PropTypes.string.isRequired,
+        director: PropTypes.string.isRequired,
+        starring: PropTypes.arrayOf(PropTypes.string).isRequired,
+        runTime: PropTypes.string.isRequired,
+        genre: PropTypes.string.isRequired,
+        releaseYear: PropTypes.number.isRequired,
+        rating: PropTypes.number.isRequired,
+        votes: PropTypes.number.isRequired,
+        description: PropTypes.string.isRequired,
+        reviews: PropTypes.arrayOf(
+            PropTypes.shape({
+              rating: PropTypes.number.isRequired,
+              date: PropTypes.string.isRequired,
+              author: PropTypes.string.isRequired,
+              text: PropTypes.string.isRequired
+            })
+        ).isRequired
+      }).isRequired
+  ).isRequired,
+  movie: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    posterUrl: PropTypes.string.isRequired,
+    bigPosterUrl: PropTypes.string.isRequired,
+    director: PropTypes.string.isRequired,
+    starring: PropTypes.arrayOf(PropTypes.string).isRequired,
+    runTime: PropTypes.string.isRequired,
+    genre: PropTypes.string.isRequired,
+    releaseYear: PropTypes.number.isRequired,
+    rating: PropTypes.number.isRequired,
+    votes: PropTypes.number.isRequired,
+    description: PropTypes.string.isRequired,
+    reviews: PropTypes.arrayOf(
+        PropTypes.shape({
+          rating: PropTypes.number.isRequired,
+          date: PropTypes.string.isRequired,
+          author: PropTypes.string.isRequired,
+          text: PropTypes.string.isRequired
+        })
+    ).isRequired
+  }).isRequired
 };
 
 export default SimilarMovies;
