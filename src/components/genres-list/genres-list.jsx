b8fd7625dc5b5114d88377b856b4fc3045588253
snpr <vaugk13@gmail.com>
2020-02-21 17:59:57 +0500

    Convert GenresList to class component

--- src/components/genres-list/genres-list.jsx
+++ src/components/genres-list/genres-list.jsx
@@ -1,26 +1,34 @@
-import React from "react";
+import React, {PureComponent} from "react";
 import {ALL_GENRES} from "../../utils/constants.js";
 
-const GenresList = ({movies}) => {
-  return (
-    <ul className="catalog__genres-list">
-      {
-        new Set([
-          ALL_GENRES,
-          ...movies.map((movie) => (
-            <li
-              className="catalog__genres-item catalog__genres-item--active"
-              key={movie.genre}
-            >
-              <a href="#" className="catalog__genres-link">
-                {movie.genre}
-              </a>
-            </li>
-          ))
-        ])
-      }
-    </ul>
-  );
-};
+class GenresList extends PureComponent {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const {movies} = this.props;
+
+    return (
+      <ul className="catalog__genres-list">
+        {
+          new Set([
+            ALL_GENRES,
+            ...movies.map((movie) => (
+              <li
+                className="catalog__genres-item catalog__genres-item--active"
+                key={movie.genre}
+              >
+                <a href="#" className="catalog__genres-link">
+                  {movie.genre}
+                </a>
+              </li>
+            ))
+          ])
+        }
+      </ul>
+    );
+  }
+}
 
 export default GenresList;
