ba0c769951c234a59a0ea3a09984255c38964055
snpr <vaugk13@gmail.com>
2020-02-24 17:49:48 +0500

    Connect ShowMoreButton component to redux

--- src/components/show-more-button/show-more-button.jsx
+++ src/components/show-more-button/show-more-button.jsx
@@ -1,13 +1,31 @@
 import React from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { ActionCreator } from "../../reducer.js";
 
-const ShowMoreButton = () => {
-  return (
+const ShowMoreButton = ({ movies, showedMovies, showMoreMovies }) => {
+  return showedMovies < movies.length ? (
     <div className="catalog__more">
-      <button className="catalog__button" type="button">
+      <button
+        className="catalog__button"
+        type="button"
+        onClick={showMoreMovies}
+      >
         Show more
       </button>
     </div>
-  );
+  ) : null;
 };
 
-export default ShowMoreButton;
+const mapStateToProps = state => ({
+  movies: state.films,
+  showedMovies: state.showedMovies
+});
+
+const mapDispatchToProps = dispatch => ({
+  showMoreMovies() {
+    dispatch(ActionCreator.showMoreMovies());
+  }
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(ShowMoreButton);
