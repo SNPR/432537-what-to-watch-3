5b94c0c5295730e8f0f0c3ed839d72d2c640f74a
snpr <vaugk13@gmail.com>
2020-02-24 17:49:22 +0500

    Add action for movie showing logic

--- src/reducer.js
+++ src/reducer.js
@@ -2,19 +2,32 @@ import {extend} from "./utils/utils.js";
 import films from "./mocks/films.js";
 import {ALL_GENRES} from "./utils/constants.js";
 
+const SHOWED_MOVIES_DEFAULT = 8;
+
 const initialState = {
   genre: ALL_GENRES,
-  films
+  films,
+  showedMovies: SHOWED_MOVIES_DEFAULT
 };
 
 const ActionType = {
-  CHANGE_GENRE: `CHANGE_GENRE`
+  CHANGE_GENRE: `CHANGE_GENRE`,
+  SHOW_MORE_MOVIES: `SHOW_MORE_MOVIES`,
+  RESET_SHOWED_MOVIES_AMOUNT: `RESET_SHOWED_MOVIES_AMOUNT`
 };
 
 const ActionCreator = {
   changeGenre: (genre = ALL_GENRES) => ({
     type: ActionType.CHANGE_GENRE,
     payload: genre
+  }),
+  showMoreMovies: () => ({
+    type: ActionType.SHOW_MORE_MOVIES,
+    payload: null
+  }),
+  resetShowedMoviesAmount: () => ({
+    type: ActionType.RESET_SHOWED_MOVIES_AMOUNT,
+    payload: null
   })
 };
 
@@ -23,6 +36,14 @@ const reducer = (state = initialState, action) => {
     case ActionType.CHANGE_GENRE:
       return extend(state, {
         genre: action.payload
+      });
+    case ActionType.SHOW_MORE_MOVIES:
+      return extend(state, {
+        showedMovies: state.showedMovies + 8
+      });
+    case ActionType.RESET_SHOWED_MOVIES_AMOUNT:
+      return extend(state, {
+        showedMovies: SHOWED_MOVIES_DEFAULT
       });
   }
 
