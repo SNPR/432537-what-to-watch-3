d1cc13df921930bd39a9ccc79180051c7d582ad9
snpr <vaugk13@gmail.com>
2020-02-24 17:54:39 +0500

    Add proptypes to ShowMoreButton component

--- src/components/show-more-button/show-more-button.jsx
+++ src/components/show-more-button/show-more-button.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import { connect } from "react-redux";
+import {connect} from "react-redux";
 import PropTypes from "prop-types";
-import { ActionCreator } from "../../reducer.js";
+import {ActionCreator} from "../../reducer.js";
 
-const ShowMoreButton = ({ movies, showedMovies, showMoreMovies }) => {
+const ShowMoreButton = ({movies, showedMovies, showMoreMovies}) => {
   return showedMovies < movies.length ? (
     <div className="catalog__more">
       <button
@@ -17,15 +17,43 @@ const ShowMoreButton = ({ movies, showedMovies, showMoreMovies }) => {
   ) : null;
 };
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state) => ({
   movies: state.films,
   showedMovies: state.showedMovies
 });
 
-const mapDispatchToProps = dispatch => ({
+const mapDispatchToProps = (dispatch) => ({
   showMoreMovies() {
     dispatch(ActionCreator.showMoreMovies());
   }
 });
+
+ShowMoreButton.propTypes = {
+  movies: PropTypes.arrayOf(
+      PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        posterUrl: PropTypes.string.isRequired,
+        bigPosterUrl: PropTypes.string.isRequired,
+        director: PropTypes.string.isRequired,
+        starring: PropTypes.arrayOf(PropTypes.string).isRequired,
+        runTime: PropTypes.string.isRequired,
+        genre: PropTypes.string.isRequired,
+        releaseYear: PropTypes.number.isRequired,
+        rating: PropTypes.number.isRequired,
+        votes: PropTypes.number.isRequired,
+        description: PropTypes.string.isRequired,
+        reviews: PropTypes.arrayOf(
+            PropTypes.shape({
+              rating: PropTypes.number.isRequired,
+              date: PropTypes.string.isRequired,
+              author: PropTypes.string.isRequired,
+              text: PropTypes.string.isRequired
+            })
+        ).isRequired
+      }).isRequired
+  ).isRequired,
+  showMoreMovies: PropTypes.func.isRequired,
+  showedMovies: PropTypes.number.isRequired
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(ShowMoreButton);
