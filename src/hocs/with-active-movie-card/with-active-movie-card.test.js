0b82c2ffc92075976f031b38adab8351e5111a3e
snpr <vaugk13@gmail.com>
2020-02-26 13:28:29 +0500

    Add test for withActiveMovieCard HOC

---
+++ src/hocs/with-active-movie-card/with-active-movie-card.test.js
@@ -0,0 +1,194 @@
+import React from "react";
+import {configure, shallow} from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import withActiveMovieCard from "./with-active-movie-card.jsx";
+
+configure({adapter: new Adapter()});
+
+const MockComponent = () => <div />;
+const MockComponentWrapped = withActiveMovieCard(MockComponent);
+
+const films = [
+  {
+    name: `Movie name`,
+    posterUrl: `https://poster-url.com`,
+    bigPosterUrl: `https://image-url.com/1.jpg`,
+    director: `Director Name`,
+    starring: [`Actor 1`, `Actor 2`, `Actor 3`],
+    runTime: `1h 00m`,
+    genre: `Movie Genre`,
+    releaseYear: 2000,
+    rating: 8.9,
+    votes: 4235,
+    description: `Movie description`,
+    reviews: [
+      {
+        rating: 9,
+        date: `November 10, 2019`,
+        author: `Dmitriy`,
+        text: `Review text`
+      }
+    ]
+  },
+  {
+    name: `Movie name`,
+    posterUrl: `https://poster-url.com`,
+    bigPosterUrl: `https://image-url.com/1.jpg`,
+    director: `Director Name`,
+    starring: [`Actor 1`, `Actor 2`, `Actor 3`],
+    runTime: `1h 00m`,
+    genre: `Movie Genre`,
+    releaseYear: 2000,
+    rating: 8.9,
+    votes: 4235,
+    description: `Movie description`,
+    reviews: [
+      {
+        rating: 9,
+        date: `November 10, 2019`,
+        author: `Dmitriy`,
+        text: `Review text`
+      }
+    ]
+  },
+  {
+    name: `Movie name`,
+    posterUrl: `https://poster-url.com`,
+    bigPosterUrl: `https://image-url.com/1.jpg`,
+    director: `Director Name`,
+    starring: [`Actor 1`, `Actor 2`, `Actor 3`],
+    runTime: `1h 00m`,
+    genre: `Movie Genre`,
+    releaseYear: 2000,
+    rating: 8.9,
+    votes: 4235,
+    description: `Movie description`,
+    reviews: [
+      {
+        rating: 9,
+        date: `November 10, 2019`,
+        author: `Dmitriy`,
+        text: `Review text`
+      }
+    ]
+  },
+  {
+    name: `Movie name`,
+    posterUrl: `https://poster-url.com`,
+    bigPosterUrl: `https://image-url.com/1.jpg`,
+    director: `Director Name`,
+    starring: [`Actor 1`, `Actor 2`, `Actor 3`],
+    runTime: `1h 00m`,
+    genre: `Movie Genre`,
+    releaseYear: 2000,
+    rating: 8.9,
+    votes: 4235,
+    description: `Movie description`,
+    reviews: [
+      {
+        rating: 9,
+        date: `November 10, 2019`,
+        author: `Dmitriy`,
+        text: `Review text`
+      }
+    ]
+  },
+  {
+    name: `Movie name`,
+    posterUrl: `https://poster-url.com`,
+    bigPosterUrl: `https://image-url.com/1.jpg`,
+    director: `Director Name`,
+    starring: [`Actor 1`, `Actor 2`, `Actor 3`],
+    runTime: `1h 00m`,
+    genre: `Movie Genre`,
+    releaseYear: 2000,
+    rating: 8.9,
+    votes: 4235,
+    description: `Movie description`,
+    reviews: [
+      {
+        rating: 9,
+        date: `November 10, 2019`,
+        author: `Dmitriy`,
+        text: `Review text`
+      }
+    ]
+  },
+  {
+    name: `Movie name`,
+    posterUrl: `https://poster-url.com`,
+    bigPosterUrl: `https://image-url.com/1.jpg`,
+    director: `Director Name`,
+    starring: [`Actor 1`, `Actor 2`, `Actor 3`],
+    runTime: `1h 00m`,
+    genre: `Movie Genre`,
+    releaseYear: 2000,
+    rating: 8.9,
+    votes: 4235,
+    description: `Movie description`,
+    reviews: [
+      {
+        rating: 9,
+        date: `November 10, 2019`,
+        author: `Dmitriy`,
+        text: `Review text`
+      }
+    ]
+  },
+  {
+    name: `Movie name`,
+    posterUrl: `https://poster-url.com`,
+    bigPosterUrl: `https://image-url.com/1.jpg`,
+    director: `Director Name`,
+    starring: [`Actor 1`, `Actor 2`, `Actor 3`],
+    runTime: `1h 00m`,
+    genre: `Movie Genre`,
+    releaseYear: 2000,
+    rating: 8.9,
+    votes: 4235,
+    description: `Movie description`,
+    reviews: [
+      {
+        rating: 9,
+        date: `November 10, 2019`,
+        author: `Dmitriy`,
+        text: `Review text`
+      }
+    ]
+  },
+  {
+    name: `Movie name`,
+    posterUrl: `https://poster-url.com`,
+    bigPosterUrl: `https://image-url.com/1.jpg`,
+    director: `Director Name`,
+    starring: [`Actor 1`, `Actor 2`, `Actor 3`],
+    runTime: `1h 00m`,
+    genre: `Movie Genre`,
+    releaseYear: 2000,
+    rating: 8.9,
+    votes: 4235,
+    description: `Movie description`,
+    reviews: [
+      {
+        rating: 9,
+        date: `November 10, 2019`,
+        author: `Dmitriy`,
+        text: `Review text`
+      }
+    ]
+  }
+];
+
+it(`Should change selected movie ID on mouseover/mouseout`, () => {
+  const wrapper = shallow(
+      <MockComponentWrapped movies={films} onMovieCardClick={() => {}} />
+  );
+
+  expect(wrapper.props().selectedMovieId).toEqual(null);
+
+  wrapper.props().onMovieCardMouseOver(0);
+  expect(wrapper.props().selectedMovieId).toEqual(0);
+
+  wrapper.props().onMovieCardMouseOut();
+  expect(wrapper.props().selectedMovieId).toEqual(null);
+});
