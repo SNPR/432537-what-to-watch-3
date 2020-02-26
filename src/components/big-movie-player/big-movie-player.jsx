4d12a580b0a0d9e15e49827c3493175c395b4ab7
snpr <vaugk13@gmail.com>
2020-02-26 23:20:19 +0500

    Render play/pause button depending on playing status

--- src/components/big-movie-player/big-movie-player.jsx
+++ src/components/big-movie-player/big-movie-player.jsx
@@ -73,10 +73,21 @@ class BigMoviePlayer extends PureComponent {
 
           <div className="player__controls-row">
             <button type="button" className="player__play">
-              <svg viewBox="0 0 19 19" width="19" height="19">
-                <use xlinkHref="#play-s" />
-              </svg>
-              <span>Play</span>
+              {this.state.isPlaying ? (
+                <>
+                  <svg viewBox="0 0 14 21" width="14" height="21">
+                    <use xlinkHref="#pause"></use>
+                  </svg>
+                  <span>Pause</span>
+                </>
+              ) : (
+                <>
+                  <svg viewBox="0 0 19 19" width="19" height="19">
+                    <use xlinkHref="#play-s" />
+                  </svg>
+                  <span>Play</span>
+                </>
+              )}
             </button>
             <div className="player__name">Transpotting</div>
 
