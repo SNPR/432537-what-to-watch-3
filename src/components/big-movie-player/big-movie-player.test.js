16e7e21d391627a619e88bb472a5e77c9e09a0ef
snpr <vaugk13@gmail.com>
2020-02-27 23:40:14 +0500

    Add snapshot test for BigMoviePlayer component

--- src/components/big-movie-player/big-movie-player.test.js
+++ src/components/big-movie-player/big-movie-player.test.js
@@ -1,140 +1,13 @@
 import React from "react";
 import renderer from "react-test-renderer";
-import { BigMoviePlayer } from "./big-movie-player.jsx";
-
-const SHOWED_MOVIES_DEFAULT = 8;
+import BigMoviePlayer from "./big-movie-player.jsx";
 
 const films = [
   {
     name: `Movie name`,
     posterUrl: `https://poster-url.com`,
     bigPosterUrl: `https://image-url.com/1.jpg`,
-    director: `Director Name`,
-    starring: [`Actor 1`, `Actor 2`, `Actor 3`],
-    runTime: `1h 00m`,
-    genre: `Movie Genre`,
-    releaseYear: 2000,
-    rating: 8.9,
-    votes: 4235,
-    description: `Movie description`,
-    reviews: [
-      {
-        rating: 9,
-        date: `November 10, 2019`,
-        author: `Dmitriy`,
-        text: `Review text`
-      }
-    ]
-  },
-  {
-    name: `Movie name`,
-    posterUrl: `https://poster-url.com`,
-    bigPosterUrl: `https://image-url.com/1.jpg`,
-    director: `Director Name`,
-    starring: [`Actor 1`, `Actor 2`, `Actor 3`],
-    runTime: `1h 00m`,
-    genre: `Movie Genre`,
-    releaseYear: 2000,
-    rating: 8.9,
-    votes: 4235,
-    description: `Movie description`,
-    reviews: [
-      {
-        rating: 9,
-        date: `November 10, 2019`,
-        author: `Dmitriy`,
-        text: `Review text`
-      }
-    ]
-  },
-  {
-    name: `Movie name`,
-    posterUrl: `https://poster-url.com`,
-    bigPosterUrl: `https://image-url.com/1.jpg`,
-    director: `Director Name`,
-    starring: [`Actor 1`, `Actor 2`, `Actor 3`],
-    runTime: `1h 00m`,
-    genre: `Movie Genre`,
-    releaseYear: 2000,
-    rating: 8.9,
-    votes: 4235,
-    description: `Movie description`,
-    reviews: [
-      {
-        rating: 9,
-        date: `November 10, 2019`,
-        author: `Dmitriy`,
-        text: `Review text`
-      }
-    ]
-  },
-  {
-    name: `Movie name`,
-    posterUrl: `https://poster-url.com`,
-    bigPosterUrl: `https://image-url.com/1.jpg`,
-    director: `Director Name`,
-    starring: [`Actor 1`, `Actor 2`, `Actor 3`],
-    runTime: `1h 00m`,
-    genre: `Movie Genre`,
-    releaseYear: 2000,
-    rating: 8.9,
-    votes: 4235,
-    description: `Movie description`,
-    reviews: [
-      {
-        rating: 9,
-        date: `November 10, 2019`,
-        author: `Dmitriy`,
-        text: `Review text`
-      }
-    ]
-  },
-  {
-    name: `Movie name`,
-    posterUrl: `https://poster-url.com`,
-    bigPosterUrl: `https://image-url.com/1.jpg`,
-    director: `Director Name`,
-    starring: [`Actor 1`, `Actor 2`, `Actor 3`],
-    runTime: `1h 00m`,
-    genre: `Movie Genre`,
-    releaseYear: 2000,
-    rating: 8.9,
-    votes: 4235,
-    description: `Movie description`,
-    reviews: [
-      {
-        rating: 9,
-        date: `November 10, 2019`,
-        author: `Dmitriy`,
-        text: `Review text`
-      }
-    ]
-  },
-  {
-    name: `Movie name`,
-    posterUrl: `https://poster-url.com`,
-    bigPosterUrl: `https://image-url.com/1.jpg`,
-    director: `Director Name`,
-    starring: [`Actor 1`, `Actor 2`, `Actor 3`],
-    runTime: `1h 00m`,
-    genre: `Movie Genre`,
-    releaseYear: 2000,
-    rating: 8.9,
-    votes: 4235,
-    description: `Movie description`,
-    reviews: [
-      {
-        rating: 9,
-        date: `November 10, 2019`,
-        author: `Dmitriy`,
-        text: `Review text`
-      }
-    ]
-  },
-  {
-    name: `Movie name`,
-    posterUrl: `https://poster-url.com`,
-    bigPosterUrl: `https://image-url.com/1.jpg`,
+    trailerUrl: `https://image-url.com/1.mp4`,
     director: `Director Name`,
     starring: [`Actor 1`, `Actor 2`, `Actor 3`],
     runTime: `1h 00m`,
@@ -155,7 +28,22 @@ const films = [
 ];
 
 it(`Should render BigMoviePlayer component`, () => {
-  const tree = renderer.create(<BigMoviePlayer></BigMoviePlayer>).toJSON();
+  const tree = renderer
+    .create(
+        <BigMoviePlayer
+          movie={films[0]}
+          autoPlay={false}
+          muted={true}
+          isPlaying={false}
+          getElapsedTime={() => {}}
+          getPlaybackProgress={() => {}}
+          onPlayButtonClick={() => {}}
+          onFullscreenButtonClick={() => {}}
+          onExitButtonClick={() => {}}
+          videoRef={React.createRef()}
+        />
+    )
+    .toJSON();
 
   expect(tree).toMatchSnapshot();
 });
