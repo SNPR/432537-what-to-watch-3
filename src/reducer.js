5400a05b8858fd25f7f380ca9dcb2d1b309a7b09
snpr <vaugk13@gmail.com>
2020-03-04 13:08:42 +0500

    Add action creator and operation for movies fetching

--- src/reducer.js
+++ src/reducer.js
@@ -1,19 +1,29 @@
 import {extend} from "./utils/utils.js";
 import films from "./mocks/films.js";
 import {ALL_GENRES} from "./utils/constants.js";
+import {normalizeData} from "./utils/utils.js";
 
 const SHOWED_MOVIES_DEFAULT = 8;
 
 const initialState = {
   genre: ALL_GENRES,
-  films,
+  films: [],
   showedMovies: SHOWED_MOVIES_DEFAULT
 };
 
 const ActionType = {
   CHANGE_GENRE: `CHANGE_GENRE`,
   SHOW_MORE_MOVIES: `SHOW_MORE_MOVIES`,
-  RESET_SHOWED_MOVIES_AMOUNT: `RESET_SHOWED_MOVIES_AMOUNT`
+  RESET_SHOWED_MOVIES_AMOUNT: `RESET_SHOWED_MOVIES_AMOUNT`,
+  GET_MOVIES: `GET_MOVIES`
+};
+
+export const Operation = {
+  getMovies: () => (dispatch, getState, api) => {
+    return api.get(`/films`).then((response) => {
+      dispatch(ActionCreator.getMovies(normalizeData(response.data)));
+    });
+  }
 };
 
 const ActionCreator = {
@@ -28,6 +38,10 @@ const ActionCreator = {
   resetShowedMoviesAmount: () => ({
     type: ActionType.RESET_SHOWED_MOVIES_AMOUNT,
     payload: null
+  }),
+  getMovies: (movies) => ({
+    type: ActionType.GET_MOVIES,
+    payload: movies
   })
 };
 
@@ -44,6 +58,10 @@ const reducer = (state = initialState, action) => {
     case ActionType.RESET_SHOWED_MOVIES_AMOUNT:
       return extend(state, {
         showedMovies: SHOWED_MOVIES_DEFAULT
+      });
+    case ActionType.GET_MOVIES:
+      return extend(state, {
+        films: action.payload
       });
   }
 
