46f175a43fb03e3e70819be09112b757622db74f
snpr <vaugk13@gmail.com>
2020-03-04 15:06:44 +0500

    Remove propTypes from App component

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,5 +1,4 @@
 import React, {PureComponent} from "react";
-import PropTypes from "prop-types";
 import {Switch, Route, BrowserRouter} from "react-router-dom";
 import Main from "../main/main.jsx";
 import MoviePage from "../movie-page/movie-page.jsx";
@@ -27,7 +26,6 @@ class App extends PureComponent {
   }
 
   _renderApp() {
-    const {name, genre, releaseYear, movies} = this.props;
     const {selectedMovie, isBigMoviePlayerVisible} = this.state;
 
     if (selectedMovie !== null) {
@@ -43,11 +41,6 @@ class App extends PureComponent {
 
     return (
       <Main
-        movie={selectedMovie || movies[0]}
-        name={name}
-        genre={genre}
-        releaseYear={releaseYear}
-        movies={movies}
         onMovieCardClick={this.movieCardClickHandler}
         isBigMoviePlayerVisible={isBigMoviePlayerVisible}
         onVisibilityChange={this.handleVisibility}
@@ -67,34 +60,5 @@ class App extends PureComponent {
     );
   }
 }
-
-App.propTypes = {
-  name: PropTypes.string.isRequired,
-  genre: PropTypes.string.isRequired,
-  releaseYear: PropTypes.number.isRequired,
-  movies: PropTypes.arrayOf(
-      PropTypes.shape({
-        name: PropTypes.string.isRequired,
-        posterUrl: PropTypes.string.isRequired,
-        bigPosterUrl: PropTypes.string.isRequired,
-        director: PropTypes.string.isRequired,
-        starring: PropTypes.arrayOf(PropTypes.string).isRequired,
-        runTime: PropTypes.string.isRequired,
-        genre: PropTypes.string.isRequired,
-        releaseYear: PropTypes.number.isRequired,
-        rating: PropTypes.number.isRequired,
-        votes: PropTypes.number.isRequired,
-        description: PropTypes.string.isRequired,
-        reviews: PropTypes.arrayOf(
-            PropTypes.shape({
-              rating: PropTypes.number.isRequired,
-              date: PropTypes.string.isRequired,
-              author: PropTypes.string.isRequired,
-              text: PropTypes.string.isRequired
-            })
-        ).isRequired
-      }).isRequired
-  ).isRequired
-};
 
 export default App;
