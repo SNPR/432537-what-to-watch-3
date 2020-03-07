584d552ed14e7ee5207ab3ffcdd8825c96507191
snpr <vaugk13@gmail.com>
2020-03-07 22:43:36 +0500

    Redirect to "/" after comment adding

--- src/components/add-review/add-review.jsx
+++ src/components/add-review/add-review.jsx
@@ -3,6 +3,7 @@ import {connect} from "react-redux";
 import PropTypes from "prop-types";
 import {getSelectedMovie} from "../../reducer/state/selectors.js";
 import {Operation} from "../../reducer/data/data";
+import {Redirect} from "react-router-dom";
 
 class AddReview extends PureComponent {
   constructor(props) {
@@ -12,6 +13,10 @@ class AddReview extends PureComponent {
     this.commentRef = createRef();
 
     this.handleSubmit = this.handleSubmit.bind(this);
+
+    this.state = {
+      commentAdded: false
+    };
   }
 
   handleSubmit(evt) {
@@ -23,150 +28,155 @@ class AddReview extends PureComponent {
       rating: this.submitFormRef.current.rating.value,
       comment: this.commentRef.current.value
     });
+
+    this.setState({commentAdded: true});
   }
 
   render() {
     const {movie} = this.props;
 
     return (
-      <section className="movie-card movie-card--full">
-        <div className="movie-card__header">
-          <div className="movie-card__bg">
-            <img src={movie.bigPosterUrl} alt={movie.name} />
-          </div>
-
-          <h1 className="visually-hidden">WTW</h1>
-
-          <header className="page-header">
-            <div className="logo">
-              <a href="main.html" className="logo__link">
-                <span className="logo__letter logo__letter--1">W</span>
-                <span className="logo__letter logo__letter--2">T</span>
-                <span className="logo__letter logo__letter--3">W</span>
-              </a>
+      <>
+        {(this.state.commentAdded || !this.props.movie) && <Redirect to="/" />}
+        <section className="movie-card movie-card--full">
+          <div className="movie-card__header">
+            <div className="movie-card__bg">
+              <img src={movie.bigPosterUrl} alt={movie.name} />
             </div>
 
-            <nav className="breadcrumbs">
-              <ul className="breadcrumbs__list">
-                <li className="breadcrumbs__item">
-                  <a href="movie-page.html" className="breadcrumbs__link">
-                    {movie.name}
-                  </a>
-                </li>
-                <li className="breadcrumbs__item">
-                  <a className="breadcrumbs__link">Add review</a>
-                </li>
-              </ul>
-            </nav>
+            <h1 className="visually-hidden">WTW</h1>
 
-            <div className="user-block">
-              <div className="user-block__avatar">
-                <img
-                  src="img/avatar.jpg"
-                  alt="User avatar"
-                  width="63"
-                  height="63"
-                />
+            <header className="page-header">
+              <div className="logo">
+                <a href="main.html" className="logo__link">
+                  <span className="logo__letter logo__letter--1">W</span>
+                  <span className="logo__letter logo__letter--2">T</span>
+                  <span className="logo__letter logo__letter--3">W</span>
+                </a>
               </div>
+
+              <nav className="breadcrumbs">
+                <ul className="breadcrumbs__list">
+                  <li className="breadcrumbs__item">
+                    <a href="movie-page.html" className="breadcrumbs__link">
+                      {movie.name}
+                    </a>
+                  </li>
+                  <li className="breadcrumbs__item">
+                    <a className="breadcrumbs__link">Add review</a>
+                  </li>
+                </ul>
+              </nav>
+
+              <div className="user-block">
+                <div className="user-block__avatar">
+                  <img
+                    src="img/avatar.jpg"
+                    alt="User avatar"
+                    width="63"
+                    height="63"
+                  />
+                </div>
+              </div>
+            </header>
+
+            <div className="movie-card__poster movie-card__poster--small">
+              <img
+                src={movie.posterUrl}
+                alt={movie.name}
+                width="218"
+                height="327"
+              />
             </div>
-          </header>
-
-          <div className="movie-card__poster movie-card__poster--small">
-            <img
-              src={movie.posterUrl}
-              alt={movie.name}
-              width="218"
-              height="327"
-            />
           </div>
-        </div>
 
-        <div className="add-review">
-          <form
-            action="#"
-            className="add-review__form"
-            onSubmit={this.handleSubmit}
-            ref={this.submitFormRef}
-          >
-            <div className="rating">
-              <div className="rating__stars">
-                <input
-                  className="rating__input"
-                  id="star-1"
-                  type="radio"
-                  name="rating"
-                  value="1"
-                />
-                <label className="rating__label" htmlFor="star-1">
-                  Rating 1
-                </label>
+          <div className="add-review">
+            <form
+              action="#"
+              className="add-review__form"
+              onSubmit={this.handleSubmit}
+              ref={this.submitFormRef}
+            >
+              <div className="rating">
+                <div className="rating__stars">
+                  <input
+                    className="rating__input"
+                    id="star-1"
+                    type="radio"
+                    name="rating"
+                    value="1"
+                  />
+                  <label className="rating__label" htmlFor="star-1">
+                    Rating 1
+                  </label>
 
-                <input
-                  className="rating__input"
-                  id="star-2"
-                  type="radio"
-                  name="rating"
-                  value="2"
-                />
-                <label className="rating__label" htmlFor="star-2">
-                  Rating 2
-                </label>
+                  <input
+                    className="rating__input"
+                    id="star-2"
+                    type="radio"
+                    name="rating"
+                    value="2"
+                  />
+                  <label className="rating__label" htmlFor="star-2">
+                    Rating 2
+                  </label>
 
-                <input
-                  className="rating__input"
-                  id="star-3"
-                  type="radio"
+                  <input
+                    className="rating__input"
+                    id="star-3"
+                    type="radio"
                     name="rating"
                     value="3"
                     defaultChecked
                   />
                   <label className="rating__label" htmlFor="star-3">
                     Rating 3
-                </label>
+                  </label>
 
-                <input
-                  className="rating__input"
-                  id="star-4"
-                  type="radio"
-                  name="rating"
-                  value="4"
-                />
-                <label className="rating__label" htmlFor="star-4">
-                  Rating 4
-                </label>
+                  <input
+                    className="rating__input"
+                    id="star-4"
+                    type="radio"
+                    name="rating"
+                    value="4"
+                  />
+                  <label className="rating__label" htmlFor="star-4">
+                    Rating 4
+                  </label>
 
-                <input
-                  className="rating__input"
-                  id="star-5"
-                  type="radio"
-                  name="rating"
-                  value="5"
-                />
-                <label className="rating__label" htmlFor="star-5">
-                  Rating 5
-                </label>
+                  <input
+                    className="rating__input"
+                    id="star-5"
+                    type="radio"
+                    name="rating"
+                    value="5"
+                  />
+                  <label className="rating__label" htmlFor="star-5">
+                    Rating 5
+                  </label>
+                </div>
               </div>
-            </div>
 
-            <div className="add-review__text">
-              <textarea
-                className="add-review__textarea"
-                name="review-text"
-                id="review-text"
-                placeholder="Review text"
-                ref={this.commentRef}
-                minLength="50"
-                maxLength="400"
-              />
-              <div className="add-review__submit">
-                <button className="add-review__btn" type="submit">
-                  Post
-                </button>
+              <div className="add-review__text">
+                <textarea
+                  className="add-review__textarea"
+                  name="review-text"
+                  id="review-text"
+                  placeholder="Review text"
+                  ref={this.commentRef}
+                  minLength="50"
+                  maxLength="400"
+                />
+                <div className="add-review__submit">
+                  <button className="add-review__btn" type="submit">
+                    Post
+                  </button>
+                </div>
               </div>
-            </div>
-          </form>
-        </div>
-      </section>
+            </form>
+          </div>
+        </section>
+      </>
     );
   }
 }
