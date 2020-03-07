e3f57490e2b47f271ffd9d56338da721e4dcd193
snpr <vaugk13@gmail.com>
2020-03-07 18:05:06 +0500

    Use redux in App component for selected movie id toggling

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -3,22 +3,25 @@ import {Switch, Route, BrowserRouter} from "react-router-dom";
 import Main from "../main/main.jsx";
 import MoviePage from "../movie-page/movie-page.jsx";
 import PropTypes from "prop-types";
+import AddReview from "../add-review/add-review.jsx";
+import {connect} from "react-redux";
+import {getSelectedMovie} from "../../reducer/state/selectors.js";
+import {ActionCreator} from "../../reducer/state/state.js";
 
 class App extends PureComponent {
   constructor(props) {
     super(props);
 
     this.state = {
-      selectedMovie: null,
       isBigMoviePlayerVisible: false
     };
     this.movieCardClickHandler = this.movieCardClickHandler.bind(this);
     this.handleVisibility = this.handleVisibility.bind(this);
   }
 
-  movieCardClickHandler(selectedMovie) {
-    this.setState({selectedMovie});
-    this.props.getComments(selectedMovie.id);
+  movieCardClickHandler(selectedMovieId) {
+    this.props.changeSelectedMovieId(selectedMovieId);
+    this.props.getComments(selectedMovieId);
   }
 
   handleVisibility() {
@@ -28,9 +31,10 @@ class App extends PureComponent {
   }
 
   _renderApp() {
-    const {selectedMovie, isBigMoviePlayerVisible} = this.state;
+    const {isBigMoviePlayerVisible} = this.state;
+    const {selectedMovie} = this.props;
 
-    if (selectedMovie !== null) {
+    if (selectedMovie) {
       return (
         <MoviePage
           movie={selectedMovie}
@@ -57,14 +61,47 @@ class App extends PureComponent {
           <Route exact path="/">
             {this._renderApp()}
           </Route>
+          <Route exact path="/dev-add-review">
+            <AddReview />
+          </Route>
         </Switch>
       </BrowserRouter>
     );
   }
 }
 
+const mapStateToProps = (state) => ({
+  selectedMovie: getSelectedMovie(state)
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  changeSelectedMovieId(id) {
+    dispatch(ActionCreator.changeSelectedMovieId(id));
+  }
+});
+
 App.propTypes = {
-  getComments: PropTypes.func.isRequired
+  getComments: PropTypes.func.isRequired,
+  changeSelectedMovieId: PropTypes.func.isRequired,
+  selectedMovie: PropTypes.shape({
+    name: PropTypes.string,
+    posterUrl: PropTypes.string,
+    previewUrl: PropTypes.string,
+    bigPosterUrl: PropTypes.string,
+    backgroundColor: PropTypes.string,
+    description: PropTypes.string,
+    rating: PropTypes.number,
+    votes: PropTypes.number,
+    director: PropTypes.string,
+    starring: PropTypes.arrayOf(PropTypes.string),
+    runTime: PropTypes.string,
+    genre: PropTypes.string,
+    releaseYear: PropTypes.number,
+    id: PropTypes.number,
+    isFavorite: PropTypes.bool,
+    videoUrl: PropTypes.string,
+    trailerUrl: PropTypes.string
+  }).isRequired
 };
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
