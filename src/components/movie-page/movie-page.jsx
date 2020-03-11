92f4d610e1cc38b1d832bba817089ec13472090b
snpr <vaugk13@gmail.com>
2020-03-11 14:49:33 +0500

    Add link to '/mylist' on the MoviePage component

--- src/components/movie-page/movie-page.jsx
+++ src/components/movie-page/movie-page.jsx
@@ -51,14 +51,16 @@ const MoviePage = ({
             </div>
 
             <div className="user-block">
-              <div className="user-block__avatar">
-                <img
-                  src="img/avatar.jpg"
-                  alt="User avatar"
-                  width="63"
-                  height="63"
-                />
-              </div>
+              <Link to={AppRoute.MY_LIST}>
+                <div className="user-block__avatar">
+                  <img
+                    src="img/avatar.jpg"
+                    alt="User avatar"
+                    width="63"
+                    height="63"
+                  />
+                </div>
+              </Link>
             </div>
           </header>
 
