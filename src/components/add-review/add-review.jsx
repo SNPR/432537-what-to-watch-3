e61b64c47a9c816a86be6373312a4d9b901207eb
snpr <vaugk13@gmail.com>
2020-03-11 22:52:38 +0500

    Fix avatar path in AddReview component

--- src/components/add-review/add-review.jsx
+++ src/components/add-review/add-review.jsx
@@ -103,7 +103,7 @@ class AddReview extends PureComponent {
                 <div className="user-block__avatar">
                   <Link to={AppRoute.MY_LIST}>
                     <img
-                      src="img/avatar.jpg"
+                      src="/img/avatar.jpg"
                       alt="User avatar"
                       width="63"
                       height="63"
