c2e109d09cc84b1bd4ba39af28d39145ef2b19d2
snpr <vaugk13@gmail.com>
2020-03-12 16:20:24 +0500

    Convert MoviePage to class component

--- src/components/movie-page/movie-page.jsx
+++ src/components/movie-page/movie-page.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {PureComponent} from "react";
 import PropTypes from "prop-types";
 import Tabs from "../tabs/tabs.jsx";
 import SimilarMovies from "../similar-movies/similar-movies.jsx";
@@ -14,156 +14,165 @@ import {ActionCreator} from "../../reducer/state/state.js";
 import {getSelectedMovie} from "../../reducer/state/selectors.js";
 import {Operation as DataOperation} from "../../reducer/data/data.js";
 
-const MoviePage = ({
-  movie,
-  onMovieCardClick,
-  movies,
-  authorizationStatus,
-  addMovieToMyList,
-  removeMovieFromMyList,
-  id,
-  changeSelectedMovieId,
-  getComments
-}) => {
-  changeSelectedMovieId(id);
-  getComments(id);
+class MoviePage extends PureComponent {
+  constructor(props) {
+    super(props);
+  }
 
-  return movie ? (
-    <>
-      <section className="movie-card movie-card--full">
-        <div className="movie-card__hero">
-          <div className="movie-card__bg">
-            <img src={movie.bigPosterUrl} alt={movie.name} />
+  componentDidMount() {
+    const {id, changeSelectedMovieId, getComments} = this.props;
+
+    changeSelectedMovieId(id);
+    getComments(id);
+  }
+
+  render() {
+    const {
+      movie,
+      onMovieCardClick,
+      movies,
+      authorizationStatus,
+      addMovieToMyList,
+      removeMovieFromMyList
+    } = this.props;
+
+    return movie ? (
+      <>
+        <section className="movie-card movie-card--full">
+          <div className="movie-card__hero">
+            <div className="movie-card__bg">
+              <img src={movie.bigPosterUrl} alt={movie.name} />
+            </div>
+
+            <h1 className="visually-hidden">WTW</h1>
+
+            <header className="page-header movie-card__head">
+              <div className="logo">
+                <Link to={AppRoute.ROOT} className="logo__link">
+                  <span className="logo__letter logo__letter--1">W</span>
+                  <span className="logo__letter logo__letter--2">T</span>
+                  <span className="logo__letter logo__letter--3">W</span>
+                </Link>
+              </div>
+
+              <div className="user-block">
+                <Link to={AppRoute.MY_LIST}>
+                  <div className="user-block__avatar">
+                    <img
+                      src="/img/avatar.jpg"
+                      alt="User avatar"
+                      width="63"
+                      height="63"
+                    />
+                  </div>
+                </Link>
+              </div>
+            </header>
+
+            <div className="movie-card__wrap">
+              <div className="movie-card__desc">
+                <h2 className="movie-card__title">{movie.name}</h2>
+                <p className="movie-card__meta">
+                  <span className="movie-card__genre">{movie.genre}</span>
+                  <span className="movie-card__year">{movie.releaseYear}</span>
+                </p>
+
+                <div className="movie-card__buttons">
+                  <button
+                    className="btn btn--play movie-card__button"
+                    type="button"
+                    onClick={() =>
+                      history.push(
+                          `${AppRoute.FILMS}/${movie.id}${AppRoute.PLAYER}`
+                      )
+                    }
+                  >
+                    <svg viewBox="0 0 19 19" width="19" height="19">
+                      <use xlinkHref="#play-s"></use>
+                    </svg>
+                    <span>Play</span>
+                  </button>
+                  <button
+                    className="btn btn--list movie-card__button"
+                    type="button"
+                    onClick={() => {
+                      if (movie.isFavorite) {
+                        removeMovieFromMyList(movie.id);
+                      } else {
+                        addMovieToMyList(movie.id);
+                      }
+                    }}
+                  >
+                    {movie.isFavorite ? (
+                      <svg viewBox="0 0 18 14" width="18" height="14">
+                        <use xlinkHref="#in-list"></use>
+                      </svg>
+                    ) : (
+                      <svg viewBox="0 0 19 20" width="19" height="20">
+                        <use xlinkHref="#add"></use>
+                      </svg>
+                    )}
+                    <span>My list</span>
+                  </button>
+                  {authorizationStatus === AuthorizationStatus.AUTH && (
+                    <Link
+                      to={`${AppRoute.FILMS}/${movie.id}${AppRoute.ADD_REVIEW}`}
+                      className="btn movie-card__button"
+                    >
+                      Add review
+                    </Link>
+                  )}
+                </div>
+              </div>
+            </div>
           </div>
 
-          <h1 className="visually-hidden">WTW</h1>
+          <div className="movie-card__wrap movie-card__translate-top">
+            <div className="movie-card__info">
+              <div className="movie-card__poster movie-card__poster--big">
+                <img
+                  src={movie.posterUrl}
+                  alt={movie.name}
+                  width="218"
+                  height="327"
+                />
+              </div>
 
-          <header className="page-header movie-card__head">
+              <Tabs movie={movie} />
+            </div>
+          </div>
+        </section>
+        <div className="page-content">
+          <section className="catalog catalog--like-this">
+            <h2 className="catalog__title">More like this</h2>
+
+            <SimilarMovies
+              movies={movies}
+              movie={movie}
+              onMovieCardClick={onMovieCardClick}
+            />
+          </section>
+
+          <footer className="page-footer">
             <div className="logo">
-              <Link to={AppRoute.ROOT} className="logo__link">
+              <Link to={AppRoute.ROOT} className="logo__link logo__link--light">
                 <span className="logo__letter logo__letter--1">W</span>
                 <span className="logo__letter logo__letter--2">T</span>
                 <span className="logo__letter logo__letter--3">W</span>
               </Link>
             </div>
 
-            <div className="user-block">
-              <Link to={AppRoute.MY_LIST}>
-                <div className="user-block__avatar">
-                  <img
-                    src="/img/avatar.jpg"
-                    alt="User avatar"
-                    width="63"
-                    height="63"
-                  />
-                </div>
-              </Link>
+            <div className="copyright">
+              <p>© 2019 What to watch Ltd.</p>
             </div>
-          </header>
-
-          <div className="movie-card__wrap">
-            <div className="movie-card__desc">
-              <h2 className="movie-card__title">{movie.name}</h2>
-              <p className="movie-card__meta">
-                <span className="movie-card__genre">{movie.genre}</span>
-                <span className="movie-card__year">{movie.releaseYear}</span>
-              </p>
-
-              <div className="movie-card__buttons">
-                <button
-                  className="btn btn--play movie-card__button"
-                  type="button"
-                  onClick={() =>
-                    history.push(
-                        `${AppRoute.FILMS}/${movie.id}${AppRoute.PLAYER}`
-                    )
-                  }
-                >
-                  <svg viewBox="0 0 19 19" width="19" height="19">
-                    <use xlinkHref="#play-s"></use>
-                  </svg>
-                  <span>Play</span>
-                </button>
-                <button
-                  className="btn btn--list movie-card__button"
-                  type="button"
-                  onClick={() => {
-                    if (movie.isFavorite) {
-                      removeMovieFromMyList(movie.id);
-                    } else {
-                      addMovieToMyList(movie.id);
-                    }
-                  }}
-                >
-                  {movie.isFavorite ? (
-                    <svg viewBox="0 0 18 14" width="18" height="14">
-                      <use xlinkHref="#in-list"></use>
-                    </svg>
-                  ) : (
-                    <svg viewBox="0 0 19 20" width="19" height="20">
-                      <use xlinkHref="#add"></use>
-                    </svg>
-                  )}
-                  <span>My list</span>
-                </button>
-                {authorizationStatus === AuthorizationStatus.AUTH && (
-                  <Link
-                    to={`${AppRoute.FILMS}/${movie.id}${AppRoute.ADD_REVIEW}`}
-                    className="btn movie-card__button"
-                  >
-                    Add review
-                  </Link>
-                )}
-              </div>
-            </div>
-          </div>
+          </footer>
         </div>
-
-        <div className="movie-card__wrap movie-card__translate-top">
-          <div className="movie-card__info">
-            <div className="movie-card__poster movie-card__poster--big">
-              <img
-                src={movie.posterUrl}
-                alt={movie.name}
-                width="218"
-                height="327"
-              />
-            </div>
-
-            <Tabs movie={movie} />
-          </div>
-        </div>
-      </section>
-      <div className="page-content">
-        <section className="catalog catalog--like-this">
-          <h2 className="catalog__title">More like this</h2>
-
-          <SimilarMovies
-            movies={movies}
-            movie={movie}
-            onMovieCardClick={onMovieCardClick}
-          />
-        </section>
-
-        <footer className="page-footer">
-          <div className="logo">
-            <Link to={AppRoute.ROOT} className="logo__link logo__link--light">
-              <span className="logo__letter logo__letter--1">W</span>
-              <span className="logo__letter logo__letter--2">T</span>
-              <span className="logo__letter logo__letter--3">W</span>
-            </Link>
-          </div>
-
-          <div className="copyright">
-            <p>© 2019 What to watch Ltd.</p>
-          </div>
-        </footer>
-      </div>
-    </>
-  ) : (
-    <h1>Loading...</h1>
-  );
-};
+      </>
+    ) : (
+      <h1>Loading...</h1>
+    );
+  }
+}
 
 MoviePage.propTypes = {
   movie: PropTypes.shape({
