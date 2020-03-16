bbd466009126916a81356528b97697339b9c7dd5
snpr <vaugk13@gmail.com>
2020-03-16 16:14:16 +0500

    Add prop type for SimilarMovies component

--- src/components/similar-movies/similar-movies.tsx
+++ src/components/similar-movies/similar-movies.tsx
@@ -1,13 +1,13 @@
 import * as React from "react";
 import MoviesList from "../movies-list/movies-list";
-import PropTypes from "prop-types";
 import withActiveMovieCard from "../../hocs/with-active-movie-card/with-active-movie-card";
+import { Movie } from "../../types";
 
-const MAX_SIMILAR_MOVIES_AMOUNT = 4;
+const MAX_SIMILAR_MOVIES_AMOUNT: number = 4;
 
 const MoviesListWrapped = withActiveMovieCard(MoviesList);
 
-const getSimilarMovies = (movies, movie) => {
+const getSimilarMovies = (movies: Movie[], movie: Movie): Movie[] => {
   return movies
     .filter(
       similarMovie =>
@@ -16,57 +16,23 @@ const getSimilarMovies = (movies, movie) => {
     .slice(0, MAX_SIMILAR_MOVIES_AMOUNT);
 };
 
-const SimilarMovies = ({ movies, movie, onMovieCardClick }) => {
+type SimilarMoviesProps = {
+  movies: Movie[];
+  movie: Movie;
+  onMovieCardClick: (id: number | string) => void;
+};
+
+const SimilarMovies: React.FunctionComponent<SimilarMoviesProps> = (
+  props: SimilarMoviesProps
+) => {
+  const { movies, movie, onMovieCardClick } = props;
+
   return (
     <MoviesListWrapped
       movies={getSimilarMovies(movies, movie)}
       onMovieCardClick={onMovieCardClick}
     />
   );
-};
-
-SimilarMovies.propTypes = {
-  movies: PropTypes.arrayOf(
-    PropTypes.shape({
-      name: PropTypes.string,
-      posterUrl: PropTypes.string,
-      previewUrl: PropTypes.string,
-      bigPosterUrl: PropTypes.string,
-      backgroundColor: PropTypes.string,
-      description: PropTypes.string,
-      rating: PropTypes.number,
-      votes: PropTypes.number,
-      director: PropTypes.string,
-      starring: PropTypes.arrayOf(PropTypes.string),
-      runTime: PropTypes.string,
-      genre: PropTypes.string,
-      releaseYear: PropTypes.number,
-      id: PropTypes.number,
-      isFavorite: PropTypes.bool,
-      videoUrl: PropTypes.string,
-      trailerUrl: PropTypes.string
-    }).isRequired
-  ).isRequired,
-  movie: PropTypes.shape({
-    name: PropTypes.string,
-    posterUrl: PropTypes.string,
-    previewUrl: PropTypes.string,
-    bigPosterUrl: PropTypes.string,
-    backgroundColor: PropTypes.string,
-    description: PropTypes.string,
-    rating: PropTypes.number,
-    votes: PropTypes.number,
-    director: PropTypes.string,
-    starring: PropTypes.arrayOf(PropTypes.string),
-    runTime: PropTypes.string,
-    genre: PropTypes.string,
-    releaseYear: PropTypes.number,
-    id: PropTypes.number,
-    isFavorite: PropTypes.bool,
-    videoUrl: PropTypes.string,
-    trailerUrl: PropTypes.string
-  }).isRequired,
-  onMovieCardClick: PropTypes.func.isRequired
 };
 
 export default SimilarMovies;
