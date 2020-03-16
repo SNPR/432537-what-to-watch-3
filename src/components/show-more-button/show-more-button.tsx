225ac60b77d4cc631eb9792873ce20c24bd73d2c
snpr <vaugk13@gmail.com>
2020-03-16 15:58:54 +0500

    Add prop type for ShowMoreButton component

--- src/components/show-more-button/show-more-button.tsx
+++ src/components/show-more-button/show-more-button.tsx
@@ -1,18 +1,26 @@
 import * as React from "react";
 import { connect } from "react-redux";
-import PropTypes from "prop-types";
 import { ActionCreator } from "../../reducer/state/state";
 import { getMovies, getMoviesByGenre } from "../../reducer/data/selectors";
 import { getGenre, getShowedMovies } from "../../reducer/state/selectors";
 import { ALL_GENRES, SHOWED_MOVIES_DEFAULT } from "../../utils/constants";
+import { Movie } from "../../types";
 
-const ShowMoreButton = ({
-  movies,
-  showedMovies,
-  showMoreMovies,
-  filteredMovies,
-  genre
-}) => {
+type ShowMoreButtonProps = {
+  movies: Movie[];
+  filteredMovies: Movie[];
+  showMoreMovies: () => {
+    type: string;
+    payload: null;
+  };
+  showedMovies: number;
+  genre: string;
+};
+
+const ShowMoreButton: React.FunctionComponent<ShowMoreButtonProps> = (
+  props: ShowMoreButtonProps
+) => {
+  const { movies, showedMovies, showMoreMovies, filteredMovies, genre } = props;
   return (filteredMovies.length > SHOWED_MOVIES_DEFAULT &&
     genre !== ALL_GENRES) ||
     (showedMovies < movies.length && genre === ALL_GENRES) ? (
@@ -26,54 +34,6 @@ const ShowMoreButton = ({
       </button>
     </div>
   ) : null;
-};
-
-ShowMoreButton.propTypes = {
-  movies: PropTypes.arrayOf(
-    PropTypes.shape({
-      name: PropTypes.string,
-      posterUrl: PropTypes.string,
-      previewUrl: PropTypes.string,
-      bigPosterUrl: PropTypes.string,
-      backgroundColor: PropTypes.string,
-      description: PropTypes.string,
-      rating: PropTypes.number,
-      votes: PropTypes.number,
-      director: PropTypes.string,
-      starring: PropTypes.arrayOf(PropTypes.string),
-      runTime: PropTypes.string,
-      genre: PropTypes.string,
-      releaseYear: PropTypes.number,
-      id: PropTypes.number,
-      isFavorite: PropTypes.bool,
-      videoUrl: PropTypes.string,
-      trailerUrl: PropTypes.string
-    })
-  ).isRequired,
-  filteredMovies: PropTypes.arrayOf(
-    PropTypes.shape({
-      name: PropTypes.string,
-      posterUrl: PropTypes.string,
-      previewUrl: PropTypes.string,
-      bigPosterUrl: PropTypes.string,
-      backgroundColor: PropTypes.string,
-      description: PropTypes.string,
-      rating: PropTypes.number,
-      votes: PropTypes.number,
-      director: PropTypes.string,
-      starring: PropTypes.arrayOf(PropTypes.string),
-      runTime: PropTypes.string,
-      genre: PropTypes.string,
-      releaseYear: PropTypes.number,
-      id: PropTypes.number,
-      isFavorite: PropTypes.bool,
-      videoUrl: PropTypes.string,
-      trailerUrl: PropTypes.string
-    })
-  ).isRequired,
-  showMoreMovies: PropTypes.func.isRequired,
-  showedMovies: PropTypes.number.isRequired,
-  genre: PropTypes.string.isRequired
 };
 
 const mapStateToProps = state => ({
