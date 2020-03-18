1918ddafb4517366110ffa8dba829b31292b82bf
snpr <vaugk13@gmail.com>
2020-03-18 13:01:18 +0500

    Add getAvatarUrl selector

--- src/reducer/user/selectors.js
+++ src/reducer/user/selectors.js
@@ -3,3 +3,7 @@ import Namespace from "../namespace";
 export const getAuthorizationStatus = (state) => {
   return state[Namespace.USER].authorizationStatus;
 };
+
+export const getAvatarUrl = (state) => {
+  return state[Namespace.USER].avatarUrl;
+};
